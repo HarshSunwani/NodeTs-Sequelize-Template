2de17879ee62fd74960a05e2fb59c8ba12c88918
HarshSunwani <harshsunwani11@gmail.com>
2023-07-16 00:06:48 +0530

    config structure changed

--- src/api/models/index.ts
+++ src/api/models/index.ts
@@ -1,14 +1,31 @@
-import { Sequelize } from "sequelize";
+import fs from "fs";
+import path from "path";
+import Sequelizer, { Sequelize } from "sequelize";
 import config from "../../config/variable"
 
+const basename = path.basename(__filename);
 const db: any = {};
 
 const sequelize = new Sequelize(
-    config.database as string,
-    config.username as string,
-    config.password,
-    config as object
+    config.database.database as string,
+    config.database.username as string,
+    config.database.password as string,
+    config.database.options as object
 );
+
+fs.readdirSync(__dirname)
+    .filter((file: string) => {
+        return (
+            file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".ts"
+        );
+    })
+    .forEach((file: any) => {
+        const model = require(path.join(__dirname, file))(
+            sequelize,
+            Sequelizer.DataTypes
+        );
+        db[model.name] = model;
+    });
 
 Object.keys(db).forEach((modelName) => {
     if (db[modelName].associate) {
--- src/config/variable.ts
+++ src/config/variable.ts
@@ -3,36 +3,54 @@ require("dotenv").config();
 const environmentVariables = {
     env: process.env.NODE_ENV,
     development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
+        database: {
+            username: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME,
+            options: {
+                host: process.env.DB_HOST,
+                dialect: "mssql",
+                pool: {
+                    max: 5,
+                    min: 0,
+                    idle: 10000,
+                    acquire: 30000,
+                }
+            },
+        },
+        port: process.env.DEV_PORT,
         applicationurl: process.env.APPLICATION_URL,
         apiurl: process.env.API_URL,
-        dialect: "mssql",
         emailUser: process.env.EMAIL_USER,
         emailPassword: process.env.EMAIL_PASSWORD,
         fromEmail: process.env.FROM_EMAIL,
     },
     production: {
-        username: process.env.PROD_DB_USER,
-        password: process.env.PROD_DB_PASS,
-        database: process.env.PROD_DB_NAME,
-        host: process.env.PROD_DB_HOST,
+        database: {
+            username: process.env.PROD_DB_USER,
+            password: process.env.PROD_DB_PASS,
+            database: process.env.PROD_DB_NAME,
+            options: {
+                host: process.env.PROD_DB_HOST,
+                dialect: "mssql",
+                pool: {
+                    max: 5,
+                    min: 0,
+                    idle: 10000,
+                    acquire: 30000,
+                }
+            },
+        },
+        port: process.env.PROD_PORT,
         applicationurl: process.env.PROD_APPLICATION_URL,
         apiurl: process.env.PROD_API_URL,
-        dialect: "mssql",
         emailUser: process.env.PROD_EMAIL_USER,
         emailPassword: process.env.PROD_EMAIL_PASSWORD,
     },
 };
 
 const config = environmentVariables.env === "development"
-? environmentVariables.development
-: environmentVariables.production;
-
-export const ports = environmentVariables.env === "development"
-? process.env.DEV_PORT
-: process.env.PROD_PORT;
+    ? environmentVariables.development
+    : environmentVariables.production;
 
 export default config;
